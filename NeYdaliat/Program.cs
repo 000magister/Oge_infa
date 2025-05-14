1750c09a005e6b8e77498190b3765179dd88476a
timur <timur@DESKTOP-GA1H7DB>
2025-05-14 18:22:49 +0300

    Добавьте файлы проекта.

---
+++ NeYdaliat/Program.cs
@@ -0,0 +1,15 @@
+﻿var b = Convert.ToInt32(Console.ReadLine());
+var k = 0;
+var sum = 0;
+while (b != 0)
+{
+    k++;
+    if (b > 0 && b < 256 && b % 2 == 0)
+    {
+        sum += b;
+    }
+    b = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine(k);
+Console.WriteLine(sum);
